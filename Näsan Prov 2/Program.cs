95ca1c6ef7e12b191fca37cce0238f7c6816a1f5
Benjani11 <60339652+Benjani11@users.noreply.github.com>
2021-05-31 14:08:41 +0200

    Start

---
+++ Näsan Prov 2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Näsan_Prov_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
